fc0494110ad6746bbefee3698924ad44cad3790b
beatriz <108158611+bcanutoo@users.noreply.github.com>
2025-11-28 19:15:17 -0300

    interface mensagem

---
+++ ex05.java
@@ -0,0 +1,16 @@
+interface Mensagem {
+    void enviar(String texto);
+}
+
+class Email implements Mensagem {
+    public void enviar(String texto) {
+        System.out.println("Enviando e-mail: " + texto + " !!");
+    }
+}
+
+class PrincipalMsg {
+    public static void main(String[] args) {
+        Mensagem m = new Email();
+        m.enviar("Olá teste de mensagem");
+    }
+}
